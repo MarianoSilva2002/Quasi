cb92dbc3329fd6ff65050339792ecd7475eb9490
ApocaypseTS <90819235+ApocaypseTS@users.noreply.github.com>
2022-06-22 01:48:57 -0500

    Repository 1

---
+++ src/main/java/pe/edu/upc/spring/repository/IAlumno_cursosRepository.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.spring.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.spring.model.Alumno_cursos;
+
+@Repository
+public interface IAlumno_cursosRepository extends JpaRepository<Alumno_cursos, Integer>{
+	
+}
