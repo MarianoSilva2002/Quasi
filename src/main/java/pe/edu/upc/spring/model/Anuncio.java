c295e7a1508e8c4a368bf393626f243bc5042a8a
PhoenixLuxor <90794804+PhoenixLuxor@users.noreply.github.com>
2022-06-21 20:13:22 -0500

    Arreglo de cambio Model v3.0
    v3.0

--- src/main/java/pe/edu/upc/spring/model/Anuncio.java
+++ src/main/java/pe/edu/upc/spring/model/Anuncio.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.spring.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
@@ -20,25 +23,39 @@ public class Anuncio implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idAnuncio;
 	
-	@NotEmpty(message="El titulo del anuncio es necesario")
-	@Column(name="Titulo", nullable = false, length = 60)
-	private String Titulo;
+	@NotEmpty(message="El nombre del anuncio es necesario")
+	@Column(name="Nombre", nullable = false, length = 60)
+	private String Nombre;
 	
-	@NotEmpty(message="La descripcion del titulo es necesario")
+	@NotEmpty(message="La descripcion del anuncio es necesario")
 	@Column(name="Descripcion", nullable = false, length = 200)
 	private String Descripcion;
+	
+	@NotEmpty(message="La fecha del anuncio es necesaria")
+	@Column(name="Fechaanuncio", nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date Fechaanuncio;
+	
+	@NotEmpty(message="La hora del anuncio es necesaria")
+	@Column(name="Horaanuncio", nullable = false)
+	@Temporal(TemporalType.TIME)
+	private Date Horaanuncio;
 
 	public Anuncio() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Anuncio(int idAnuncio, @NotEmpty(message = "El titulo del anuncio es necesario") String titulo,
-			@NotEmpty(message = "La descripcion del titulo es necesario") String descripcion) {
+	public Anuncio(int idAnuncio, @NotEmpty(message = "El nombre del anuncio es necesario") String nombre,
+			@NotEmpty(message = "La descripcion del anuncio es necesario") String descripcion,
+			@NotEmpty(message = "La fecha del anuncio es necesaria") Date fechaanuncio,
+			@NotEmpty(message = "La hora del anuncio es necesaria") Date horaanuncio) {
 		super();
 		this.idAnuncio = idAnuncio;
-		Titulo = titulo;
+		Nombre = nombre;
 		Descripcion = descripcion;
+		Fechaanuncio = fechaanuncio;
+		Horaanuncio = horaanuncio;
 	}
 
 	public int getIdAnuncio() {
@@ -49,12 +66,12 @@ public class Anuncio implements Serializable{
 		this.idAnuncio = idAnuncio;
 	}
 
-	public String getTitulo() {
-		return Titulo;
+	public String getNombre() {
+		return Nombre;
 	}
 
-	public void setTitulo(String titulo) {
-		Titulo = titulo;
+	public void setNombre(String nombre) {
+		Nombre = nombre;
 	}
 
 	public String getDescripcion() {
@@ -64,5 +81,21 @@ public class Anuncio implements Serializable{
 	public void setDescripcion(String descripcion) {
 		Descripcion = descripcion;
 	}
-	
+
+	public Date getFechaanuncio() {
+		return Fechaanuncio;
+	}
+
+	public void setFechaanuncio(Date fechaanuncio) {
+		Fechaanuncio = fechaanuncio;
+	}
+
+	public Date getHoraanuncio() {
+		return Horaanuncio;
+	}
+
+	public void setHoraanuncio(Date horaanuncio) {
+		Horaanuncio = horaanuncio;
+	}
+
 }
